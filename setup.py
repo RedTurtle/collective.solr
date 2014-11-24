234adc0902dc83a96d25a8fa00b6109799b4238e
Andrea Cecchi <andrea.cecchi85@gmail.com>
2014-11-24 12:00:11 +0100

    Back to development: 4.0.2-rer2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '4.0.2-rer1'
+version = '4.0.2-rer2.dev0'
 long_description = \
     open("README.rst").read() + '\n' + open('CHANGES.rst').read()
 
